f1f583b44f10d5eb3e9f267c9f0cde50cd65372c
‘chizw’ <chizw@didihu.com.cn>
2024-03-22 18:21:17 +0800

    [+]代码优化

--- kafka_service/funasr_consumer.py
+++ kafka_service/funasr_consumer.py
@@ -19,43 +19,6 @@ consumer_new = KafkaConsumer(
 )
 log.info("启动2")
 
-# 创建连接池对象
-connection_pool = pooling.MySQLConnectionPool(
-    pool_name="mypool",
-    pool_size=32,
-    pool_reset_session=True,
-    host=ConfigInfo.host,
-    port=ConfigInfo.port,
-    database=ConfigInfo.database,
-    user=ConfigInfo.user,
-    password=ConfigInfo.password,
-    autocommit=True,
-)
-
-log.info("启动3")
-
-
-# 从连接池获取连接
-def get_connection():
-    log.info("connection_pool size:" + str(connection_pool._cnx_queue.qsize()))
-    # 从连接池获取连接
-    return connection_pool.get_connection()
-
-
-def execute_sql(sql):
-    try:
-        conn = get_connection()
-        cursor = conn.cursor()
-        cursor.execute(sql)
-        conn.commit()
-        log.info("sql 执行结束")
-    except Exception as msg:
-        log.error("sql执行Exception: " + str(msg))
-    finally:
-        cursor.close()
-        conn.close()
-
-
 class MultiThreadKafka(object):
 
     def __init__(self):
