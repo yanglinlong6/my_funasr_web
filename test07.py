9f879307083b3bb5b1415f0bf8ba3decff865f92
杨林龙 <yangll@didihu.com.cn>
2024-07-09 16:27:48 +0800

    添加样例

--- test07.py
+++ test07.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import time
 from funasr import AutoModel
 
@@ -7,22 +8,15 @@ import funasr_service
 # use vad, punc, spk or not as you need
 
 model = AutoModel(
-
-    # model="paraformer-zh",
-    # model="paraformer-zh-spk",
-    model="iic/speech_paraformer-large-vad-punc-spk_asr_nat-zh-cn",
-    # model="iic/SenseVoiceSmall",
-    model_revision="v2.0.4",
-    vad_model="fsmn-vad",
-    # punc_model="ct-punc-c",
-    punc_model="ct-punc",
-    spk_model="cam++",
+    # model="iic/speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch",
+    model="iic/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch",
+    # model="iic/speech_conformer_asr_nat-zh-cn-16k-aishell2-vocab5212-pytorch",
+    vad_model="iic/speech_fsmn_vad_zh-cn-16k-common-pytorch",
+    # punc_model="iic/punc_ct-transformer_zh-cn-common-vocab272727-pytorch",
+    punc_model="iic/punc_ct-transformer_cn-en-common-vocab471067-large",
+    spk_model="iic/speech_campplus_sv_zh-cn_16k-common",
     # spk_model="iic/speech_eres2net_sv_zh-cn_16k-common",
     # spk_model="iic/speech_campplus_speaker-diarization_common",
-    # spk_model_revision="v1.0.0",
-    # spk_model="iic/speech_eres2net-large_speaker-diarization_common",
-    # openai_model="Whisper-large-v3",
-    ncpu=8,
 )
 
 
@@ -40,6 +34,11 @@ res = model.generate(
     input="https://glsk-oss.oss-cn-shenzhen.aliyuncs.com/quality/merged_17198870994551719886349986.mp3",
     batch_size_s=300,
     hotword="问界 80\n电瓶 100\n保修 100\n问界店 100\nM7\nM5\nM9",
+    # return_raw_text=True,     # return raw text recognition  splited by space of equal length with timestamp
+    preset_spk_num=2,  # preset speaker num for speaker cluster model
+    sentence_timestamp=True,  # return sentence level information when spk_model is not given
+    # decoding_ctc_weight=0.0,
+    # batch_size=1,
 )
 print(f"res:{res}")
 sentence_info = res[0]["sentence_info"]
@@ -54,25 +53,17 @@ for one in sentence_info:
     # print(one["end"] - one["start"])
     # print(one["text"])
     # print(one["spk"])
-    # print(f'{one["end"] - one["start"]}; {one["text"]}; {one["spk"]}')
-    total_time = total_time + (one["end"] - one["start"])
-    if spk == one["spk"]:
-        content = content + one["text"]
-    else:
-        text = f"角色{spk + 1} : {content} -- 时长:{total_time}ms"
-        print(text)
-        write_text(text)
-        total_time = 0
-        content = one["text"]
-        spk = one["spk"]
-
-if content is not None:
-    text = f"角色{spk + 1} : {content} -- 时长:{total_time}ms"
+    spk = 1 if one["spk"] == 0 else 2
+    text = f'[{one["end"] - one["start"]}] {spk}: {one["text"]}'
     print(text)
     write_text(text)
 
-res = funasr_service.fine_grained_transform_output(sentence_info)
-print(res)
+    # seconds = one['Start'] // 1000
+    # lineTime = str(datetime.timedelta(seconds=seconds))
+    # if len(lineTime) < 8:
+    #     lineTime = '0' + lineTime
+    # wordLine = f'[{lineTime}] {spk}:'
+
 
 
 consuming_end_time = time.perf_counter()
